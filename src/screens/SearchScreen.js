d539a831767bbe84ee4d2d3e09fff558bf4bc3c8
preintercede <dylanw@uci.edu>
2021-07-16 18:27:01 -0700

    configured axios and handled some error handling when users fail to search

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,17 +1,40 @@
 import React, {useState} from 'react'
 import {View, Text, StyleSheet} from 'react-native'
 import SearchBar from '../components/SearchBar'
+import yelp from '../api/yelp'
 
 const SearchScreen = () => {
 
     const [term, setTerm] = useState('')
+    const [results, setResults] = useState([])
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const searchApi = async(searchTerm) => {
+        console.log('Hi there')
+        try{
+        const response = await yelp.get('/search', {
+            params: {
+                limit: 50,
+                term: searchTerm,
+                location: 'los angeles'
+            }
+        })
+        setResults(response.data.businesses)
+    } catch(err){
+        setErrorMessage('Something went wrong')
+    }
+    }
+
+
+
     return (
         <View>
             <SearchBar term={term}
-            onTermChange={newTerm=> setTerm(newTerm)}
-            onTermSubmit = {()=> console.log('term was submitted')}
+            onTermChange = {setTerm}
+            onTermSubmit = {()=>searchApi(term)}
             />
-            <Text>Search Screen</Text>
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
+            <Text>We have found {results.length} results.</Text>
         </View>
     )
 }
