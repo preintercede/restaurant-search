ad1da5b42527c3a13247d81e0fe1aa31d612c6d8
preintercede <dylanw@uci.edu>
2021-07-17 17:12:58 -0700

    added useeffect hook to reuse search results later and showed search results with restaurant names

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,30 +1,20 @@
 import React, {useState} from 'react'
 import {View, Text, StyleSheet} from 'react-native'
 import SearchBar from '../components/SearchBar'
-import yelp from '../api/yelp'
+import useResults from '../hooks/useResults'
+import ResultsList from '../components/ResultsList'
 
 const SearchScreen = () => {
 
     const [term, setTerm] = useState('')
-    const [results, setResults] = useState([])
-    const [errorMessage, setErrorMessage] = useState('')
+    const [searchApi, results, errorMessage] = useResults()
 
-    const searchApi = async(searchTerm) => {
-        console.log('Hi there')
-        try{
-        const response = await yelp.get('/search', {
-            params: {
-                limit: 50,
-                term: searchTerm,
-                location: 'los angeles'
-            }
+    const filterResultsByPride = (price) => {
+        // price === '$' || '$$' || '$$$'
+        return results.filter(result => {
+            return result.price === price
         })
-        setResults(response.data.businesses)
-    } catch(err){
-        setErrorMessage('Something went wrong')
     }
-    }
-
 
 
     return (
@@ -35,6 +25,9 @@ const SearchScreen = () => {
             />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We have found {results.length} results.</Text>
+            <ResultsList results={filterResultsByPride('$')} title="Cost Effective"/>
+            <ResultsList results={filterResultsByPride('$$')} title="Bit Pricier"/>
+            <ResultsList results={filterResultsByPride('$$$')} title="Big Spender"/>
         </View>
     )
 }
